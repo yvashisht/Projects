0c227f55dcbb94614add8634036ac9258197bc1c
Yajur Vashisht <132369766+yvashisht@users.noreply.github.com>
2023-12-19 19:07:20 -0700

    Bootstrap styling

--- scheduling-app/src/components/Course.jsx
+++ scheduling-app/src/components/Course.jsx
@@ -1,5 +1,19 @@
-const Course = ({ course }) => (
-    <p>{course.term} CS {course.number} : {course.title}</p>
-);
+import './CourseList.css'
+
+const Course = ({ course }) => {
+    return (
+        <div className="card m-1 p-2">
+            <div className="card-body">
+                <h5 className="course-title">
+                    {course.term} CS {course.number}
+                </h5>
+                <p className="card-text">
+                    {course.title}
+                </p>
+            </div>
+            <div className="card-footer bg-transparents"> {course.meets}</div>
+        </div>
+    );
+};
 
 export default Course;
--- scheduling-app/src/components/CourseList.jsx
+++ scheduling-app/src/components/CourseList.jsx
@@ -1,9 +1,14 @@
 import Course from "./Course";
 
-const CourseList = ({ courses }) => (
-    <div>
-        {Object.entries(courses).map(([id, course]) => <Course key={id} course={course} />)}
-    </div>
-);
+const CourseList = ({ courses }) => {
+    return (
+        <div className="course-list">
+            {Object.entries(courses).map(([id, course]) => (
+                <Course key={id} course={course} />
+            ))}
+        </div>
+    );
+};
+
 
 export default CourseList;
